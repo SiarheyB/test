8205c12501b926a21f4d181fdf59287a7dba65c1
sergey.buzuk <sergey.buzuk@innowise-group.com>
2021-02-28 23:03:52 +0300

    initial String constant value

--- src/main/java/by/onyx/test/TestApplication.java
+++ src/main/java/by/onyx/test/TestApplication.java
@@ -2,9 +2,12 @@ package main.java.by.onyx.test;
 
 public class TestApplication {
 
+    private static final String TEST_CONSTANT = "test";
+
     public static void main(String[] args) {
         int a = 1;
         String b = "a";
+
     }
 
 }
